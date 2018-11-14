1141ce0bae03f4b95be196f95a59fa66206f2a59
wwong7132 <wwong7132@gmail.com>
2018-11-14 16:35:48 -0600

    Reduced HTML
    -Reduced usage of HTML with JavaScript.
    -Split Javascript file into two.
    -Added method to read json file.

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-var mydata = JSON.parse(data);
-alert(mydata[0].name)
+var data;
+$.getJSON( "data.json", setData);
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-	navStick()
-};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function navStick() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
+function setData(data) {
+ 	//console.log( "JSON Data: " + data.pages );
+	for (i = 0; i < data.pages.length; i++)
+	{
+/* 		//Stuff
+		console.log( "JSON: " + data.pages[i].title );
+		
+		 */
+	}
+	//document.getElementById(i).innerHTML = data.name;
+	
+}
---
+++ nav.js
@@ -0,0 +1,41 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navStick()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+$.getJSON( "data.json", generateNav);
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navStick() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
+
+//Creates Navigation Bar based on json file
+function generateNav(data) {
+	var a = document.createElement('a');
+	
+	//Gets the amount of pages and sets length
+	for (i = 0; i < data.pages.length; i++)
+	{
+		//Creates a tab for each element supplied in the json file
+		var header = document.getElementById("navbar");
+		var a = document.createElement('a');
+		a.setAttribute("href","javascript:void(0)");
+		
+		//Changes first tab to green
+		if (i == 0)
+		{
+			a.setAttribute("class", "active")
+		}
+		a.innerHTML = data.pages[i].title;
+		header.appendChild(a);
+	}
+}
