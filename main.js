34c941edf3bb47745656b35fb6ef1a3c5642c5d5
wwong7132 <wwong7132@gmail.com>
2018-11-15 15:59:19 -0600

    Page switching
    -Created basic framwork for swithing pages

--- main.js
+++ main.js
@@ -1,16 +1,103 @@
 var data;
-$.getJSON( "data.json", setData);
 
+//$.getJSON( "data.json", saveData);
 
-function setData(data) {
- 	//console.log( "JSON Data: " + data.pages );
-	for (i = 0; i < data.pages.length; i++)
-	{
-/* 		//Stuff
-		console.log( "JSON: " + data.pages[i].title );
+//Saves json file as global variable
+function saveData( callback) {
+    $.getJSON( "data.json", function (data) {
+      callback(data);
+    });
+}
+
+//Test function
+saveData(function( json ){
+	data = json;
+	//console.log( "JSON Data: " + data.pages[2] );
+});
+
+//Takes json and ID number as inputs. ID corresponds to the number in the json file
+function switchPage( data, id ) {
+	/* console.log( "setData function...");
+	console.log( "id input: " + id)
+ 	console.log( "JSON Data: " + Object.keys(data.pages[id].content[0]) ); */
 		
-		 */
-	}
-	//document.getElementById(i).innerHTML = data.name;
+	setPage( data, id);
+	//Switch case statement because of different sub categories of json file
+	//Should consider editing json to have similar subgroups...
+	/* switch( id ) {
+		case 0:
+			setPage( data, id);
+			break;
+		case 1:
+			setPage( data, id);
+			break;
+		case 2:
+			setPage( data, id);
+			break;
+		case 3:
+			setPage( data, id);
+			break;
+		case 4:
+			setPage( data, id);
+			break;
+	} */
+}
+
+function setPage( data, id ) {
+	var p = document.createElement('p');
 	
+	//Clear page before adding new content
+	clearPage();
+	
+	//Loop to display all content in the page
+	for ( i in data.pages[id].content )
+	{
+		//Creates a tab for each element supplied in the json file
+		var content = document.getElementById( "content" );
+		var h = document.createElement( 'h2' );
+		var temp;
+		h.innerHTML = data.pages[id].content[i].title;
+		h.setAttribute( "id", "content-top" );
+		
+		//Append data
+		content.appendChild( h );
+		
+		//Populate page with content under title
+		var p = document.createElement( 'p' );
+		p.innerHTML = data.pages[id].content[i].description;
+		p.setAttribute( "id", "content-mid" );
+		content.appendChild( p );
+		
+		//console.log(data.pages[id].content[i].links[0].title);
+		
+		//Fill links and other content if applicable
+		
+		//Check if links exist
+		if (data.pages[id].content[i].links){
+			//Create frame div for bottom row
+			var frame = document.createElement( 'div' );
+			frame.setAttribute( "id" , "content-bot-frame" );
+			content.appendChild( frame );
+			
+			//Create div for link placement
+			var d = document.createElement( 'div' );
+			d.setAttribute( "id" , "content-bot" + i);
+			frame.appendChild( d );
+			d = document.getElementById( "content-bot" + i );
+			
+			//Prints each link in json category
+			for ( j in data.pages[id].content[i].links )
+			{
+				var a = document.createElement( 'a' );
+				a.innerHTML = data.pages[id].content[i].links[j].title;
+				a.setAttribute( "href", data.pages[id].content[i].links[j].url)
+				a.setAttribute( "id", "links" + i);
+				d.appendChild( a );
+			}
+		}
+	}
+}
+
+function clearPage(){
+	document.getElementById( "content" ).innerHTML = ''
 }
--- nav.js
+++ nav.js
@@ -1,39 +1,38 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {navStick()};
+window.onscroll = function() { navStick() };
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementById( "navbar" );
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-$.getJSON( "data.json", generateNav);
+$.getJSON( "data.json", generateNav );
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navStick() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+  if ( window.pageYOffset >= sticky ) {
+    navbar.classList.add( "sticky" )
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove( "sticky" );
   }
 } 
 
 //Creates Navigation Bar based on json file
-function generateNav(data) {
-	var a = document.createElement('a');
-	
+function generateNav( data ) {
 	//Gets the amount of pages and sets length
-	for (i = 0; i < data.pages.length; i++)
+	for ( i = 0; i < data.pages.length; i++ )
 	{
 		//Creates a tab for each element supplied in the json file
-		var header = document.getElementById("navbar");
+		var header = document.getElementById( "navbar" );
 		var a = document.createElement('a');
-		a.setAttribute("href","javascript:void(0)");
+		a.setAttribute( "href","javascript:void(0)" );
+		a.setAttribute( "onclick", "switchPage(data," + i + " )" )
 		
 		//Changes first tab to green
 		if (i == 0)
 		{
-			a.setAttribute("class", "active")
+			a.setAttribute( "class", "active" )
 		}
 		a.innerHTML = data.pages[i].title;
 		header.appendChild(a);
